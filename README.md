fa2f3f770ca12c7d0e1e1f99ccffaf55e4a153c5
Josh Gretz <jgretz@truefit.io>
2022-01-21 10:27:14 -0500

    break out to own app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # igor-pghbeer
+
+the microservice for hosting data that backs pgh beer
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 import {NestFactory} from '@nestjs/core';
 import {PghBeerModule} from './pghbeer.module';
 
-export async function bootstrap() {
+async function bootstrap() {
   const app = await NestFactory.create(PghBeerModule);
   app.startAllMicroservices();
 }
+bootstrap();
